8badee4f5b3cef63cab6e72ae6cc6ede95ed7c4f
Larissa Soares Reis <larissasoaresreis@gmail.com>
2024-01-05 20:17:40 -0300

    Refactor button layout in StartGameScreen.js

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -10,14 +10,22 @@ const StartGameScreen = () => {
                         autoCapitalize='none'
                         autoCorrect={false} 
             />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
         padding: 16,
         marginTop:100,
         marginHorizontal:24,
@@ -39,6 +47,12 @@ const styles = StyleSheet.create({
         borderBottomWidth: 2,
         marginVertical: 8,
         textAlign: 'center',
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+    },
+    buttonContainer: {
+        flex: 1,
     },
 });
 
