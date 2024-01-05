183baa28569c877f0358e4bb7a7251cdf0f6567e
Larissa Soares Reis <larissasoaresreis@gmail.com>
2024-01-05 18:39:53 -0300

    Add dummy press functionality and styling to PrimaryButton component

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,27 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 const PrimaryButton = ({ children }) => {
+    function pressed() {
+        console.log('pressed');
+    }
     return (
-        <View>
-            <Text>{children}</Text>
-        </View>
+        <Pressable onPress={pressed}>
+            <View style={styles.container}>
+                <Text>{children}</Text>
+            </View>
+        </Pressable>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#103778',
+        borderRadius: 28,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        margin: 4,
+        elevation: 2,
+    },
+});
 
 export default PrimaryButton;
