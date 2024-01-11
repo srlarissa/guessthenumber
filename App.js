04d1ff6ae9f6cb1043a3e27b8c0d296f8f336502
Larissa Soares Reis <larissasoaresreis@gmail.com>
2024-01-11 19:45:57 -0300

    number reception on app.js and screen change

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
-import { StyleSheet, ImageBackground } from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import {  useState } from 'react';
+import {  StyleSheet, 
+          ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 
 export default function App() {
+  const [playerNumber, setPlayerNumber] = useState();
+
+  function playerPickedNumber(pickedNumber){
+    setPlayerNumber(pickedNumber)
+  }
+
+  let screen = <StartGameScreen onConfirmNumber={playerPickedNumber}  />
+
+  if(playerNumber){
+    screen = <GameScreen />
+  }
+
   return (
    <LinearGradient colors={['#012030', '#13678A']} style={styles.rootContainer}>
-      <ImageBackground  source={require('./assets/image/background.png')} 
-                        resizeMode='cover'
-                        style={styles.rootContainer}
-                        imageStyle={styles.backgroundImage}
-      >
-        <StartGameScreen />
+      <ImageBackground  source={require('./assets/image/background.png')} resizeMode='cover' style={styles.rootContainer} imageStyle={styles.backgroundImage}>
+        { screen }
       </ImageBackground>      
    </LinearGradient>
   );
