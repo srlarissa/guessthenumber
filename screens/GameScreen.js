4a716ccb7f0cd6473609d51cf7b252b9aa6a077e
Larissa Soares Reis <larissasoaresreis@gmail.com>
2024-01-11 20:32:13 -0300

    title component with dynamic text

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,19 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import PrimaryTitle from '../components/PrimaryTitle';
 
 const GameScreen = () => {
     return (
-        <View>
-            <Text>GameScreen</Text>
+        <View style={styles.screen}>
+            <PrimaryTitle>Opponent's Guess</PrimaryTitle>
         </View>
     );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen:{
+        flex:1,
+        padding:24,
+    },
+})
