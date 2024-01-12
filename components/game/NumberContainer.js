78d061e36a942d2441eb36401710c967646c178f
Larissa Soares Reis <larissasoaresreis@gmail.com>
2024-01-11 23:20:17 -0300

    add NumberContainer component

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,8 +1,31 @@
-
-const NumberContainer = () => {
+import { View, Text, StyleSheet } from 'react-native';
+import Colors from '../../constants/Colors';
+const NumberContainer = ({ children }) => {
     return(
-
+        <View style={styles.container}>
+            <Text style={styles.numberTxt}>{children}</Text>
+        </View>
     )
 }
 
 export default NumberContainer;
+
+const styles = StyleSheet.create({
+    container:{
+        width:140,
+        backgroundColor: Colors.numberBoxBg,
+        borderWidth:5,
+        borderColor: Colors.numberBox,
+        margin: 24,
+        paddingVertical:24,
+        paddingHorizontal:24,
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center'
+    },
+    numberTxt:{
+        color:Colors.numberBox,
+        fontSize: 52,
+        fontWeight:'bold',
+    }
+})
